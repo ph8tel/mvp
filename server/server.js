8305b16b05b9881f19501cbb0981088643f3e2b6
Joe Jackson <ph8tel@gmail.com>
2017-12-11 17:28:37 -0800

    basic server logic in

--- server/server.js
+++ server/server.js
@@ -4,11 +4,42 @@ const fs = require('fs')
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+var requestHandler = function (req, res) {
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World\n');
+  if ( req.method = 'GET' && req.url ==='/' ) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../index.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/alaska.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../alaska.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/merica') {
+    console.log('RAN with ', req.url);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../merica.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/mexico.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../mexico.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/canadaEast.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../canadaEast.html");
+  } else if (req.method = 'GET' && req.url === '/canadaWest.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../canadaWest.html").pipe(res);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Sorry, 404. not found')
+  }
+};
+const server = http.createServer(requestHandler);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
