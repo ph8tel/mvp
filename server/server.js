764a2a7b8c2df73ce93df93a6d4cf0c560eb590d
Joe Jackson <ph8tel@gmail.com>
2017-12-11 18:59:49 -0800

    Search routes in place

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var requestHandler = function (req, res) {
   // res.statusCode = 200;
   // res.setHeader('Content-Type', 'text/plain');
   // res.end('Hello World\n');
-  if ( req.method = 'GET' && req.url ==='/' ) {
+  if ( req.method = 'GET' && ( req.url ==='/' || req.url === '/index.html') ) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     fs.createReadStream("../index.html").pipe(res);
@@ -16,24 +16,75 @@ var requestHandler = function (req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     fs.createReadStream("../alaska.html").pipe(res);
-  } else if (req.method = 'GET' && req.url === '/merica') {
-    console.log('RAN with ', req.url);
+  } else if (req.method = 'GET' && req.url === '/merica.html') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     fs.createReadStream("../merica.html").pipe(res);
-  } else if (req.method = 'GET' && req.url === '/mexico.html') {
+  } else if (req.method = 'GET' && req.url === '/merica.html' ) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../merica.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/mexico.html' ) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     fs.createReadStream("../mexico.html").pipe(res);
   } else if (req.method = 'GET' && req.url === '/canadaEast.html') {
+    console.log('RAN with qubecqua', req.url);
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    fs.createReadStream("../canadaEast.html");
+    fs.createReadStream("../canadaEast.html").pipe(res);
   } else if (req.method = 'GET' && req.url === '/canadaWest.html') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     fs.createReadStream("../canadaWest.html").pipe(res);
-  } else {
+  }
+  //begin search requsts
+    else if (req.method = 'GET' && req.url === '/search?searchFor=mexico') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../mexico.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/search?searchFor=america') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../merica.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/search?searchFor=alaska') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../alaska.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/search?searchFor=canada') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../canadaWest.html").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/search?searchFor=quebec') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.createReadStream("../merica.html").pipe(res);
+  }
+  //begin image requests
+    else if (req.method = 'GET' && req.url === '/images/01Alaska.gif') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/gif');
+    fs.createReadStream("../images/01Alaska.gif").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/images/3-coldAmericaFrenchies.gif') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/gif');
+    fs.createReadStream("../images/3-coldAmericaFrenchies.gif").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/images/1-coldAmerica.gif') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/gif');
+    fs.createReadStream("../images/1-coldAmerica.gif").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/images/11murica.gif') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/gif');
+    fs.createReadStream("../images/11murica.gif").pipe(res);
+  } else if (req.method = 'GET' && req.url === '/images/21hotAmerica.gif') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'image/gif');
+    fs.createReadStream("../images/21hotAmerica.gif").pipe(res);
+  }
+  // 404 catch
+  else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Sorry, 404. not found')
